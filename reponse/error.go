344f4fe3da86277b4f5647297c5d8b04a4c63a3c
Jannik Schäfer <jas@zeitraum.com>
2020-05-31 14:02:42 +0200

    add some missing documentation

--- reponse/error.go
+++ reponse/error.go
@@ -1,9 +1,11 @@
 package reponse
 
+// Default Error Response
 type Error struct {
 	Base
 }
 
+// Creates a new Error Response
 func NewError(code int, message interface{}) Error {
 	data := map[string]interface{}{
 		"error":   true,
--- reponse/response.go
+++ reponse/response.go
@@ -2,19 +2,23 @@ package reponse
 
 import "github.com/gin-gonic/gin"
 
+// Default JSON Response Interface
 type Response interface {
 	Apply(c *gin.Context)
 }
 
+// Base Response Struct
 type Base struct {
 	code int
 	data interface{}
 }
 
+// Applies the response to the given gin context
 func (r Base) Apply(c *gin.Context) {
 	c.JSON(r.code, r.data)
 }
 
+// Creates a new base response object
 func NewResponse(code int, data interface{}) Base {
 	return Base{code, data}
 }
